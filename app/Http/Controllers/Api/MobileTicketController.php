b6fc9b335ccfd1a1fd62ac3303ce3f4734b39a10
powji17 <fauzii2552@gmail.com>
2026-04-23 18:44:33 +0700

    hapus use auth from mobileticketcontroller

--- app/Http/Controllers/Api/MobileTicketController.php
+++ app/Http/Controllers/Api/MobileTicketController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use App\Models\TicketLog;
-use Illuminate\Support\Facades\Auth;
 
 class MobileTicketController extends Controller
 {
